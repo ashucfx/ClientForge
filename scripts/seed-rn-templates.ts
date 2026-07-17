f0dde3e39479437961bc6def36937e262fc2154e
Ashutosh Shukla <ashutosh6471@gmail.com>
2026-07-17 18:21:21 +0530

    fix: resolve module scope ts errors in scripts

--- scripts/seed-rn-templates.ts
+++ scripts/seed-rn-templates.ts
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -166,3 +166,5 @@ main()
   .finally(async () => {
     await prisma.$disconnect();
   });
+
+export {};
